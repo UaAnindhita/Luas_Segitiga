e6f5e07574d50cb3c230729799084e750261448d
U.AniNdhiAa_Ak <141287115+UaAnindhita@users.noreply.github.com>
2023-10-05 08:55:25 +0700

    Add files via upload

---
+++ LuasSegitigaFlowgorithm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    float luas;
+    float alas;
+    float tinggi;
+    cout<<"Masukkan Alas Segitiga : \n";
+    cin >> alas;
+    cout<<"Masukkan Tinggi Segitiga : \n";
+    cin >> tinggi;
